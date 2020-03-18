67d0acd5d32418c4d7d63f2e3b93dca8f5b65f93
h316101 <weiwei.dai@honeywell.com>
2020-03-18 16:11:13 +0800

    init

---
+++ src/routers/routers.ts
@@ -0,0 +1,28 @@
+import {Home} from "../pages/home/Home";
+import {Test} from "../pages/home/test/Test";
+import {Game} from "../pages/games/components/game/Game";
+import {Clock} from "../pages/clock/Clock";
+import {RouterModel} from "../models";
+
+export const routers: RouterModel[] = [
+    {
+        path: '/home',
+        name: 'home',
+        component: Home,
+        children: [
+            {
+                path: 'test',
+                name: 'test',
+                component: Test
+            }
+        ]
+    }, {
+        path: '/game',
+        name: 'game',
+        component: Game,
+    }, {
+        path: '/clock',
+        name: 'clock',
+        component: Clock,
+    }
+];
